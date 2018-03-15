603b648dc4e75d8281499b0ca47282bd4dff2369
蘇昱銘 <jng916@hotmail.com>
2018-03-15 18:59:25 +0800

    2018/3/15 1900 昱銘 controller相關程式

---
+++ EEIT9903/src/main/java/controller/SearchController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+
+	@RequestMapping("/baseinfo/search")
+	public String setStockId(String stock_id , HttpSession session) {
+//		model.addAttribute("data", stock_id);
+		session.setAttribute("data", stock_id);
+		return "info.page";  
+	}
+}
